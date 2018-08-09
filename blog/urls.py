ca0047bbf56cf3227b27b1e6abd621151ecfeabc
patricia-777 <patricia.goncalves.unb@gmail.com>
2018-08-08 22:58:10 -0300

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,19 @@
-from django.conf.urls import url
-from . import views
+# from django.conf.urls import url
+# from . import views
+
+
+# urlpatterns = [
+#     url(r'^$', views.post_list, name='post_list'),
+#     url('post/<int:pk>/', views.post_detail, name='post_detail'),
+# ]
+
+
+from django.urls import path, include
+from django.contrib import admin
+from blog import views
 
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+	path('', views.post_list, name='post_list'),
+	path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
